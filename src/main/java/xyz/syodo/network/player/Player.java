e766b28e50c990443b9baa7d9badd8125b838b68
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-03-11 20:07:51 +0100

    Update Player.java

--- src/main/java/xyz/syodo/network/player/Player.java
+++ src/main/java/xyz/syodo/network/player/Player.java
@@ -4,12 +4,18 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.cloudburstmc.protocol.bedrock.BedrockSession;
 import org.cloudburstmc.protocol.bedrock.packet.*;
+import xyz.syodo.network.PacketHandlerPipe;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 @RequiredArgsConstructor
 public class Player {
 
-    @Getter
     protected final BedrockSession session;
+    protected final PacketHandlerPipe packetHandlerPipe;
+    protected final Map<String, Object> meta = new HashMap<>();
 
     public void sendPacket(BedrockPacket packet) {
         session.sendPacket(packet);
